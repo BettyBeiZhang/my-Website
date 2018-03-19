94449eafcdd91780a2f6007e13d1f93f7f909b7c
bei zhang <beizhang18@yahoo.com>
2018-03-19 03:50:05 -0700

    Add background video, update my experiences section

--- gulpfile.js
+++ gulpfile.js
@@ -122,6 +122,8 @@ gulp.task('browserSync', function() {
 // Dev task
 gulp.task('dev', ['css', 'js', 'browserSync'], function() {
   gulp.watch('./scss/*.scss', ['css']);
+  gulp.watch('./css/*.css', ['css']);
   gulp.watch('./js/*.js', ['js']);
   gulp.watch('./*.html', browserSync.reload);
 });
+
